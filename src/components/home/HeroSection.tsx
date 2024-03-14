590bbdf5e66e36290898f8ee57d759fc2d5b967e
signor1 <emmanuelomemgboji@gmail.com>
2024-03-14 02:27:58 +0100

    chore: hero section update

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,113 +1,39 @@
-
+import { ImageWrap } from "../atom/ImageWrap";
+import bgImage from "../../assets/slider/slider_bg.jpg";
+import shape01 from "../../assets/slider/slider_shape01.png";
+import { Text } from "../atom/Text";
+import { Button } from "../atom/Button";
 
 const HeroSection = () => {
     return (
-        <section className="slider__area slider__bg bg-center bg-cover z-[1] pt-[158px] pb-[54px] px-0 relative after:right-0 before:content-[''] before:absolute before:w-6/12 before:bg-[#45f882] before:h-[50px] before:left-0 before:bottom-0 before:clip-path-polygon-[0_0,_0_100%,_100%_100%] after:content-[''] after:absolute after:w-6/12 after:bg-[#45f882] after:h-[50px] after:left-auto after:bottom-0 after:clip-path-polygon-[100%_0,_0_100%,_100%_100%]
-        xl:before:h-[40px] xl:after:h-[40px]
-        lg:before:h-[30px] lg:after:h-[30px]
-        md:before:h-[30px] md:after:h-[30px]
-        sm:before:h-[20px] sm:after:h-[20px]
-        xsm:before:h-[20px] xsm:after:h-[20px]  
-        " data-background="assets/img/slider/slider_bg.jpg">
-            <div className="slider-activee">
-                <div className="single-slider">
-                    <div className="container custom-container">
-                        <div className="flex flex-wrap mx-[-15px] justify-between">
-                            <div
-                                className="w-6/12 basis-6/12 xxl:w-6/12 xxl:basis-6/12 xl:w-6/12 xl:basis-6/12 lg:w-6/12 lg:basis-6/12 md:w-full md:basis-full sm:w-full sm:basis-full  xsm:w-full xsm:basis-full relative px-[15px]">
-                                <div className="slider__content relative z-[2] ml-10 mr-0 mt-[135px] mb-0 xl:m-[90px_0_0_0] text-left
-                                
-                                2xl:mt-[135px] 2xl:mb-0 2xl:mx-0
-                                xl:mt-[90px]  xl:xl:mb-0 mx-0
-                                lg:m-[90px_0_0_0]
-                                md:text-center md:mt-0 md:mb-[70px] md:mx-0 
-                                sm:mt-0 sm:mb-[50px] sm:mx-0 sm:text-center
-                                xsm:mt-0 xsm:mb-[50px] xsm:mx-0 xsm:text-center
-                                ">
-                                    <h6 className="sub-title leading-none font-bold tracking-[4px] text-[20px] text-[#45f882] relative mt-0 mb-[19px] mx-0 pl-5 pr-0 py-[17px] before:content-[''] before:absolute before:bg-[url(./assets/img/slider/text\_gradient\_bg.png)] before:w-full before:h-full before:bg-no-repeat before:z-[-1] before:opacity-[0.28] before:left-0 before:top-0 font-Poppins wow fadeInUp
-                                    lg:text-base lg:pl-5 lg:pr-0 lg:py-[15px] lg:before:bg-contain
-                                    md:text-lg md:px-5 md:py-[15px] md:before:w-[297px] md:before:mx-auto  md:before:my-0 md:before:left-[86px] md:before:right-0  md:before:top-0 md:before:bg-contain
-                                    sm:text-[17px] sm:px-5 sm:py-[15px] sm:before:w-64 sm:before:mx-auto sm:before:my-0 sm:before:left-[55px] sm:before:top-0 sm:before:right-0 sm:before:bg-contain
-                                    xsm:text-[17px] xsm:px-5 xsm:py-[15px] xsm:before:w-64 xsm:before:mx-auto xsm:before:my-0 xsm:before:left-[55px] xsm:before:top-0 xsm:before:right-0 xsm:before:bg-contain
-                                    " data-wow-delay=".2s">live gaming</h6>
-                                    <h2 className="title text-[150px] leading-[0.8] mt-0 mb-[21px] mx-0 font-berlin
-                                    drop-shadow-[-1px_5px_0px_rgba(69,248,130,0.66)] wow fadeInUp
-                                    md:text-[120px]
-                                    lg:text-[94px]
-                                    xl:text-[118px]
-                        sm:text-[14vw] sm:drop-shadow-[-1px_5px_0px_rgba(69,248,130,0.66)] sm:mt-0 sm:mb-[21px] sm:mx-0
-                        xsm:text-[14vw] xsm:drop-shadow-[-1px_5px_0px_rgba(69,248,130,0.66)] xsm:mt-0 xsm:mb-[21px] xsm:mx-0
-                                    " data-wow-delay=".5s">steaming</h2>
-                                    <p className=" text-[26px] uppercase font-bold tracking-[5px] leading-[1.4] text-[#fff] m-0 wow fadeInUp
-                                    md:text-2xl
-                                    lg:text-[22px]
-                                    sm:text-xl sm:tracking-[3px]
-                                    xsm:text-xl xsm:tracking-[3px]
-                                    " data-wow-delay=".8s">video games online</p>
-                                    <div className="slider__btn mt-[33px] mb-0 mx-0 wow fadeInUp" data-wow-delay="1.2s">
-                                        <a href="contact.html" className="tg-btn-1 px-[49px] py-[15px]"><span>contact
-                                            us</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="w-6/12 basis-6/12 xxl:w-5/12 xxl:basis-5/12 xl:w-5/12 xl:basis-5/12 lg:w-6/12 lg:basis-6/12 md:w-full md:basis-full sm:w-full sm:basis-full xsm:w-full xsm:basis-full relative px-[15px]">
-                                <div className="slider__img text-center">
-                                    <img className=" mx-auto my-0 max-w-full 
-                                        md:mx-auto md:my-0
-                                        lg:ml-auto lg:mr-0 lg:my-0
-                                        sm:mx-auto sm:my-0
-                                        xsm:mx-auto xsm:my-0
-                                    " src="assets/img/slider/slider_img01.png" data-magnetic alt="img" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="slider__shapes opacity-[0.27]">
-                <img src="assets/img/slider/slider_shape01.png" alt="shape" className=" absolute animate-[shapeBlinker_1.5s_linear_infinite] opacity-0 z-[-1] left-[23%] top-[17%] delay-[0.2s]
-                md:left-[13%] md:top-[12%]
-                sm:left-[11%] sm:top-[11%]
-                xsm:left-[11%] xsm:top-[11%]
-                "/>
-                <img src="assets/img/slider/slider_shape02.png" alt="shape" className=" absolute animate-[shapeBlinker_1.5s_linear_infinite] opacity-0 z-[-1] left-[29%] bottom-[45%] delay-[0.4s]
-                md:left-[12%] md:bottom-[45%]
-                sm:left-[10%] sm:bottom-[55%]
-                xsm:left-[10%] xsm:bottom-[55%]"/>
-                <img src="assets/img/slider/slider_shape03.png" alt="shape" className=" absolute animate-[shapeBlinker_1.5s_linear_infinite] opacity-0 z-[-1] right-[47%] top-[20%] delay-[0.2s]
-                md:right-[23%] md:top-[15%]
-                sm:right-[10%] sm:top-[17%]
-                xsm:right-[10%] xsm:top-[17%]"/>
-                <img src="assets/img/slider/slider_shape04.png" alt="shape" className=" absolute animate-[shapeBlinker_1.5s_linear_infinite] opacity-0 z-[-1] right-[42%] top-[23%] delay-[0.6s]
-                xl:right-[40%] xl:top-[28%]
-                lg:right-[40%] lg:top-[28%]
-                md:right-[20%] md:top-[35%]
-                sm:right-[14%] sm:top-[33%]
-                xsm:right-[14%] xsm:top-[33%]"/>
-            </div>
-            <div className="slider__brand-wrap absolute w-full h-auto left-0 bottom-[130px]
-            
-            md:relative md:bottom-0
-            lg:bottom-[85px]
-            xl:bottom-[150px]
-            sm:relative sm:bottom-0
-            xsm:relative xsm:bottom-0
-            ">
-                <div className="container custom-container">
-                    <ul className="slider__brand-list list-wrap m-0 p-0  flex flex-wrap w-6/12 items-center gap-[20px_60px] ml-10 mr-0 my-0 lg:ml-0 xl:m-0
-                    md:w-full md:justify-center
-                    lg:w-3/5 lg:gap-[20px_30px]
-                    2xl:m-0
-                    sm:flex-nowrap sm:gap-5 sm:w-full  sm:justify-center  sm:m-0  sm:text-center
-                    xsm:flex-nowrap xsm:gap-5 xsm:w-full  xsm:justify-center  xsm:m-0  xsm:text-center
-                    ">
-                        <li><a className="block" href="#"><img src="assets/img/brand/brand_logo01.png" alt="brand" /></a></li>
-                        <li><a className="block" href="#"><img src="assets/img/brand/brand_logo02.png" alt="brand" /></a></li>
-                        <li><a className="block" href="#"><img src="assets/img/brand/brand_logo03.png" alt="brand" /></a></li>
-                    </ul>
-                </div>
-            </div>
+        <section className="bg-center lg:h-[80vh] md:h-[60vh] h-[90vh] w-full bg-cover z-[1] relative after:right-0 before:content-[''] before:absolute before:w-6/12 before:bg-[#45f882] before:h-[50px] before:left-0 before:bottom-0 before:clip-path-polygon-[0_0,_0_100%,_100%_100%] after:content-[''] after:absolute after:w-6/12 after:bg-[#45f882] after:h-[50px] after:left-auto after:bottom-0 after:clip-path-polygon-[100%_0,_0_100%,_100%_100%] xl:before:h-[40px] xl:after:h-[40px] lg:before:h-[30px] lg:after:h-[30px] md:before:h-[30px] md:after:h-[30px] sm:before:h-[20px] sm:after:h-[20px] 
+        ">
+            {/* banner */}
+            <ImageWrap image={bgImage} alt="Banner" className="w-full h-full" objectStatus="object-cover" />
+
+            {/* Start Shape */}
+            <ImageWrap image={shape01} alt="Shape01" className="absolute left-[15%] top-[17%] md:left-[13%] md:top-[12%] sm:left-[11%] sm:top-[11%] " objectStatus="object-cover" />
+
+            <ImageWrap image={shape01} alt="Shape02" className=" absolute left-[10%] bottom-[45%] md:left-[12%] md:bottom-[45%] sm:left-[10%] sm:bottom-[55%] " objectStatus="object-cover" />
+
+            <ImageWrap image={shape01} alt="Shape03" className="absolute right-[47%] top-[20%] md:right-[53%] md:top-[15%] sm:right-[10%] sm:top-[17%] " objectStatus="object-cover" />
+
+            <ImageWrap image={shape01} alt="Shape04" className="w-[80px] h-[80px] absolute right-[22%] top-[43%] xl:right-[40%] xl:top-[32%] lg:right-[40%] lg:top-[32%] md:right-[20%] md:top-[35%] sm:right-[14%] sm:top-[33%] " objectStatus="object-fill" />
+            {/* Start Shape */}
+
+            <main className="w-full h-full absolute top-0 inset-x-0 bg-gradient-to-b from-[#0f161b]/60 z-[10] flex md:flex-row flex-col px-10">
+                <aside className="flex-1 flex flex-col justify-center items-start gap-4">
+                    <Text as="h3" className="uppercase bg-gradient-to-r from-myGreen text-gray-100 px-8 py-3 rounded-md font-barlow font-bold tracking-widest text-2xl">Live Gaming</Text>
+                    <Text as="h1" className="uppercase text-8xl leading-[0.8] font-poppins font-bold drop-shadow-[-1px_5px_0px_rgba(69,248,130,0.66)]
+                         sm:drop-shadow-[-1px_5px_0px_rgba(69,248,130,0.66)] ">POKEMONING</Text>
+                    <Text as="h5" className="uppercase ">Video Games Online</Text>
+                    <Button className="slider-cta-btn text-gray-100 text-base font-bold font-barlow px-4 py-2 flex justify-center items-center">
+                        Play Now
+                    </Button>
+                </aside>
+                <aside className="flex-1 flex flex-col justify-center items-center"></aside>
+            </main>
+
         </section>
     )
 }
