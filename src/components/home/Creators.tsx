067dd4d06166c5c453bd7c434852571af0024126
signor1 <emmanuelomemgboji@gmail.com>
2024-03-14 19:31:26 +0100

    feat: creators section done

--- src/components/home/Creators.tsx
+++ src/components/home/Creators.tsx
@@ -1,10 +1,55 @@
+import { ImageWrap } from "../atom/ImageWrap"
 import MaxWrapper from "../shared/MaxWrapper"
+import Img1 from "../../assets/creators/nft_img01.jpg"
+import Img2 from "../../assets/creators/nft_img02.jpg"
+import Img3 from "../../assets/creators/nft_img03.jpg"
+import Avatar1 from "../../assets/creators/nft_avatar01.png"
+import Avatar2 from "../../assets/creators/nft_avatar02.png"
+import Avatar3 from "../../assets/creators/nft_avatar03.png"
+import { useMemo } from "react"
+import { Text } from "../atom/Text"
+import { Button } from "../atom/Button"
+import { MdArrowRightAlt } from "react-icons/md"
+import { creators } from "../../data/ContentData"
+
 
 
 const Creators = () => {
+
+    const imgList = useMemo(() => [Img1, Img2, Img3], []);
+    const avatartList = useMemo(() => [Avatar1, Avatar2, Avatar3], []);
+
     return (
-        <MaxWrapper className="w-full flex justify-center">
-            <section className="w-full grid md:grid-cols-2">Creators</section>
+        <MaxWrapper className="w-full flex justify-center py-24">
+            <section className="flex flex-wrap justify-center gap-6 w-full">
+                {
+                    creators.map((creator, index) => (
+                        <div key={index} className="w-4/12 basis-4/12 xxl:w-4/12 xxl:basis-4/12 xl:w-5/12 xl:basis-5/12 lg:w-6/12 lg:basis-6/12 md:w-9/12 md:basis-9/12 sm:w-full sm:basis-full flex items-center gap-4 p-6 bg-[#121a23] bg-[linear-gradient(0deg,#0c0e12_0%,rgba(31,41,53,0.36078)_100%)] rounded-md cursor-pointer border border-[rgba(76,76,76,0.2)] hover:border-[rgba(69,248,130,0.4)]">
+                            <ImageWrap image={imgList[index]} alt="Creator" className="w-[180px] h-[187px]" objectStatus="object-cover rounded-md" />
+                            <div className="w-full h-full flex-1 flex-col justify-center items-start">
+                                <Text as="h4" className="uppercase text-gray-100 font-bold font-barlow text-xl">{creator.title}</Text>
+                                <div className="w-full flex gap-4 items-center my-4">
+                                    <ImageWrap image={avatartList[index]} alt="Avatar" className="w-[40px] h-[40px] rounded-full" objectStatus="object-cover" />
+                                    <Text as="span" className="text-gray-400 font-medium font-barlow text-xl">{creator.createdBy}</Text>
+                                    <Text as="span" className="flex border-l-2 uppercase border-gray-500 text-sm pl-4">creator</Text>
+                                </div>
+
+                                <div className="w-full py-4 flex justify-between px-4 items-center rounded-md border border-[rgba(76,76,76,0.4)]">
+                                    <Text as="p" className="flex items-center gap-2 font-bold text-gray-100">
+                                        {creator.amount}
+                                        <Text as="span" className=" text-myYellow font-bold font-barlow text-lg">ETH</Text>
+                                    </Text>
+                                    <Button type="button" className="flex items-center gap-1 bg-myYellow hover:bg-myGreen text-myBlack px-4 rounded-md py-2">
+                                        <Text as="span" className="text-lg font-poppins">Buy</Text>
+                                        <MdArrowRightAlt className="text-2xl" />
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+
+            </section>
         </MaxWrapper>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Creators from "../components/home/Creators"
 import HeroSection from "../components/home/HeroSection"
 
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <main className="w-full flex flex-col">
             <HeroSection />
+            <Creators />
         </main>
     )
 }
