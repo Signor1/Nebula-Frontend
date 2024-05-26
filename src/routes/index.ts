43dc52c05f470329b6cfacdd49f5a5adf172f393
signor1 <emmanuelomemgboji@gmail.com>
2024-05-26 02:25:54 +0100

    feat: routed the pages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { lazy } from "react";
 
 const Home = lazy(() => import("../pages/index"));
+const About = lazy(() => import("../pages/About"));
+const Contact = lazy(() => import("../pages/Contact"));
+const Profile = lazy(() => import("../pages/Profile"));
 
 type Route = {
   path: string;
@@ -14,6 +17,21 @@ const coreRoutes: coreRoutes = [
     path: "/",
     title: "Home",
     component: Home,
+  },
+  {
+    path: "/about",
+    title: "About",
+    component: About,
+  },
+  {
+    path: "/contact",
+    title: "Contact",
+    component: Contact,
+  },
+  {
+    path: "/profile",
+    title: "Profile",
+    component: Profile,
   },
 ];
 
