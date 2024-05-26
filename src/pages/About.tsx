73c9f52033498b2f83febbda17bbeec50bb9880c
signor1 <emmanuelomemgboji@gmail.com>
2024-05-26 20:19:48 +0100

    feat: completed the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,16 @@
+import AboutUs from "../components/about/AboutUs"
 import HeroSection from "../components/about/HeroSection"
+import Services from "../components/about/Services"
+import Team from "../components/about/Team"
 
 
 const About = () => {
     return (
         <main className="w-full flex flex-col">
             <HeroSection />
+            <AboutUs />
+            <Services />
+            <Team />
         </main>
     )
 }
