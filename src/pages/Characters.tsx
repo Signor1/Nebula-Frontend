038f00bcf5d9eae2261c0ea66fef89fbe62a525f
signor1 <emmanuelomemgboji@gmail.com>
2024-05-27 00:24:50 +0100

    feat: added two more pages

---
+++ src/pages/Characters.tsx
@@ -0,0 +1,9 @@
+
+
+const Characters = () => {
+    return (
+        <main className="w-full flex flex-col"></main>
+    )
+}
+
+export default Characters
---
+++ src/pages/Purchase.tsx
@@ -0,0 +1,9 @@
+
+
+const Purchase = () => {
+    return (
+        <main className="w-full flex flex-col"></main>
+    )
+}
+
+export default Purchase
